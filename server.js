b92096353d8de3abeaea958ddb62f57d07a6052e
Jan Toebes <jtoebes@xebia.com>
2015-04-03 18:45:30 +0200

    -

---
+++ server.js
@@ -0,0 +1,6 @@
+ (function myLoop (i) {          
+    setTimeout(function () {   
+      console.log('JAAABBBA!');                
+      if (--i) myLoop(i);      //  decrement i and call myLoop again if i > 0
+    }, 3000)
+ })(100);        
