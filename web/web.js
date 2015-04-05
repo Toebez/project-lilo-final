f77749bff828f2bd8da2df5289d4ed71e8e5e6e8
Jan Toebes <jtoebes@xebia.com>
2015-04-05 12:48:10 +0200

    -

--- web/web.js
+++ web/web.js
@@ -1,39 +1,43 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  // // counter starts at 0
+  // Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    var SerialPort = Meteor.npmRequire('serialport').SerialPort;
-    var sp = new SerialPort("/dev/ttyACM0", {
-      baudrate: 9600
-    });
+  var debug = process.env.DEBUG;
 
-    sp.on("open", function () {
-      sp.on('data', function(data) {
-        console.log(data + ' ');
+  Meteor.startup(function () {
+    if (!debug) {
+      var SerialPort = Meteor.npmRequire('serialport').SerialPort;
+      var sp = new SerialPort("/dev/ttyACM0", {
+        baudrate: 9600
       });
 
-      (function myLoop (i) {
-        setTimeout(function () {
-          sp.write('s');
-          if (--i) myLoop(i);
-        }, 3000)
-      })(100);
-    });
+      sp.on("open", function () {
+        sp.on('data', function(data) {
+          console.log(data + ' ');
+        });
+
+        (function myLoop (i) {
+          setTimeout(function () {
+            sp.write('s');
+            if (--i) myLoop(i);
+          }, 3000)
+        })(100);
+      });
+    }
   });
 }
