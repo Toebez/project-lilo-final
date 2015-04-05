a4016731d607d7f34d008241a3faa8cf14365525
Jan Toebes <jtoebes@xebia.com>
2015-04-05 13:33:07 +0200

    -

--- web/web.js
+++ web/web.js
@@ -1,26 +1,38 @@
+Facts = new Mongo.Collection("facts");
+
 if (Meteor.isClient) {
-  // // counter starts at 0
-  // Session.setDefault('counter', 0);
-
-  // Template.hello.helpers({
-  //   counter: function () {
-  //     return Session.get('counter');
-  //   }
-  // });
-
-  // Template.hello.events({
-  //   'click button': function () {
-  //     // increment the counter when button is clicked
-  //     Session.set('counter', Session.get('counter') + 1);
-  //   }
-  // });
+  Template.controls.events({
+    'click button': function (event, template) {
+       Meteor.call('executeCommand', event.currentTarget.id);
+    }
+  });
+  Template.facts.helpers({
+    facts: function() {
+      return Facts.find({}, {sort: {date_created: -1}});
+    }
+  });
 }
 
 if (Meteor.isServer) {
   var debug = process.env.DEBUG;
 
+  Meteor.methods({
+    executeCommand: function(command) {
+      console.log( new Date().getTime());
+      if (debug) {
+        Facts.insert({ 
+          date_created:  new Date().getTime(),
+          text: command })
+      } else {
+      }
+    }
+  })
+
   Meteor.startup(function () {
-    if (!debug) {
+    Facts.remove({});
+    if (debug) {
+
+    } else {
       var SerialPort = Meteor.npmRequire('serialport').SerialPort;
       var sp = new SerialPort("/dev/ttyACM0", {
         baudrate: 9600
